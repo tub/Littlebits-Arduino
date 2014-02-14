9601eb082dffb8526d4294c09fc4b3dc7514b87c
Toby Cole <tub@tubs.org.uk>
2014-02-14 08:42:39 +0000

    Enable multiple notes to be held down.

--- littleBitsMidiNotes.ino
+++ littleBitsMidiNotes.ino
@@ -5,6 +5,7 @@
 #define SPEEDFACTOR 64
 #define MIN_NOTE 24
 #define MAX_NOTE MIN_NOTE+61
+#define NOTES_BUFFER 4
 
 unsigned long count = 0;
 
@@ -18,9 +19,16 @@ const byte NOTEOFF = 0x08;
 const byte CC = 0x0B;
 const byte CLOCK = 0xF;
 
-byte currentNote;
+unsigned short notePointer = 0;
+byte notes[NOTES_BUFFER];
 
 void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial || millis() > 3000) {
+    ; // wait for serial port to connect, or 3 seconds pass
+  }
+
   // put your setup code here, to run once:
   pinMode(NOTE_PIN, OUTPUT);
   digitalWrite(NOTE_PIN, LOW);
@@ -28,6 +36,9 @@ void setup() {
   digitalWrite(NOTE_TRIGGER_PIN, LOW);
   pinMode(CLOCK_PIN, OUTPUT);
   digitalWrite(CLOCK_PIN, HIGH);
+  
+  for(int i = 0; i > NOTES_BUFFER; i++)
+    notes[i] = 0;
 }
 
 void loop() {
@@ -39,18 +50,16 @@ void loop() {
       //min note = 24
       if(e.m2 >= MIN_NOTE && e.m2 <= MAX_NOTE){
         // remember which note we're playing
-        currentNote = e.m2;
-        //stretch the note number across the correct range
-        byte analogVal = map(currentNote, MIN_NOTE, MAX_NOTE, 0, 255);
-        //output as a voltage
-        analogWrite(NOTE_PIN, analogVal);
+        addNote(e.m2);
+        outputNote();
         //send a trigger pulse
         digitalWrite(NOTE_TRIGGER_PIN, HIGH);
       }
     }
-    if(e.type == NOTEOFF && e.m2 == currentNote){
-      analogWrite(NOTE_PIN, 0);
-      digitalWrite(NOTE_TRIGGER_PIN, LOW);   
+    if(e.type == NOTEOFF){
+      removeNote(e.m2);
+      outputNote();
+      digitalWrite(NOTE_TRIGGER_PIN, LOW);  
     }
     if(e.type == CC && e.m2 == 1){ 
       // cc 1
@@ -82,6 +91,63 @@ void loop() {
     }
   }//end while midi available
 }//end loop
+
+void outputNote(){
+  //stretch the note number across the correct range
+  byte note = (notePointer - 1) % NOTES_BUFFER;
+  byte analogVal;
+  if( note == 0 ){
+    analogVal = 0;
+  } 
+  else {
+    analogVal = map(notes[note], MIN_NOTE, MAX_NOTE, 0, 255);
+  }
+  Serial.print(note);
+  Serial.print(" : ");
+  Serial.println(analogVal);
+  //output as a voltage
+  analogWrite(NOTE_PIN, analogVal);
+}
+
+void addNote(byte note){
+  // just in case
+  removeNote(note);
+  notes[notePointer] = note;
+  notePointer = (notePointer + 1) % NOTES_BUFFER;
+}
+
+void removeNote(byte note){
+  boolean found = false;
+  for(int i = 0; i < NOTES_BUFFER; i++){
+    int index = (i + notePointer) % NOTES_BUFFER;
+    if(notes[index] == note){
+      found = true;
+    }
+    if(found){
+      notes[index] = notes[(index + 1) % NOTES_BUFFER];
+    }
+  }
+  if(found){
+    notePointer = (notePointer - 1) % NOTES_BUFFER;
+    notes[notePointer] = 0;
+  }
+}
+
+
+void printNotes(){
+  Serial.print("Notes[\t");
+  for(int i = 0; i < NOTES_BUFFER; i++){
+    Serial.print(notes[i]);
+    if(i == notePointer){
+      Serial.print("*");
+    }
+    Serial.print("\t");
+  }
+  Serial.println("]");
+  Serial.println(notePointer);
+}
+
+
 
 
 
